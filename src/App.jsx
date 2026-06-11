0f064e6155877b4e6ddab81d6d22357dc8042a20
meirelesgc <geu_costa@outlook.com>
2026-06-11 09:03:57 -0300

    vite + antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import './App.css'
+import { Flex, Typography, Button } from 'antd';
+import './App.css';
+
+const { Title, Paragraph } = Typography;
 
 function App() {
-  return <div>...</div>
+  return <Flex justify="center" align="center" vertical gap="middle">
+    <Typography>
+      <Title>Recepção</Title>
+      <Paragraph>Acesse o sistema.</Paragraph>
+    </Typography>
+    <Button type="primary">Entrar</Button>
+  </Flex>
 }
 
-export default App
+export default App;
